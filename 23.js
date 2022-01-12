7e925414094adfd617f6e99fdce9823226c18483
m7rk <54494161+m7rk@users.noreply.github.com>
2022-01-11 17:10:15 -0800

    23 PART 2 FIRST TRY EZ CLAP

--- 23.js
+++ 23.js
@@ -2,7 +2,7 @@ fs = require('fs');
 
 //       0    1       2         3       4         5    6 
 halls = ["",  "",     "",      "",      "",       "", ""]
-rooms =   [       "13",     "43",    "14",     "22"]
+rooms =   [       "1443",     "4323",    "1214",     "2132"]
 //                0         1       2         3
 
 function generate_legal_moves(croom,chall)
@@ -17,8 +17,12 @@ function generate_legal_moves(croom,chall)
     if(uname != "")
     {
       uval = parseFloat(uname)-1
-      // If space in the hole
-      if(croom[uval] == "" || croom[uval] == uname)
+      
+      // If space in the hole PT 1
+      //if(croom[uval] == "" || croom[uval] == uname)
+
+      // If space in the hole PT 2
+      if(croom[uval] == "" || croom[uval] == uname || croom[uval] == uname +uname || croom[uval] == uname + uname + uname)
       {
         // Suppose we are a "2", uval is "1".
         // "2"s need to get to 1+1 (2) or 1+2 (3)
@@ -60,7 +64,7 @@ function generate_legal_moves(croom,chall)
       uval = parseInt(uname)
 
       // special case, if i'm already in the room i belong in, alone or not, return.
-      if((uval-1 == r) && (croom[r] == uname || croom[r] == uname + uname))
+      if((uval-1 == r) && (croom[r] == uname || croom[r] == uname + uname || croom[r] == uname + uname + uname || croom[r] == uname + uname + uname + uname + uname))
       {
           continue
       }
@@ -102,15 +106,8 @@ function applyMove(move,oldroom,oldhall)
     // move from room to hall
     // we need to count steps, now.
     steps = 0
-    // we need one step to exit the hall (two if lone)
-    if(oldroom[move[1]].length == 2)
-    {
-      steps += 1
-    } 
-    else
-    {
-      steps += 2
-    }
+    // hardecoded 5 (use 3 for 2-variation.)
+    steps += 5 - oldroom[move[1]].length
 
     // Now, we are at the end of the hall. if greater return 1 step + how many we have to (from zero index)
     if(move[2] <= (move[1]+1))
@@ -137,6 +134,7 @@ function applyMove(move,oldroom,oldhall)
   }
   else
   {
+    // hall to room.
     
     //step calc
     steps = 0
@@ -151,14 +149,9 @@ function applyMove(move,oldroom,oldhall)
       steps += 1 + ((move[1] - 2) - move[2]) * 2
     }
 
-    if(oldroom[move[2]].length == 1)
-    {
-      steps += 1
-    } 
-    else
-    {
-      steps += 2
-    }
+
+    // hardecoded 4 (use 2 for 2-variation.)
+    steps += 4 - oldroom[move[2]].length
 
     // subtract a step if going to far rooms.
     if(move[1] == 0 || move[1] == halls.length-1)
@@ -193,7 +186,10 @@ function stringify(croom,challs)
 
 function finished(crooms)
 {
-  return (crooms[0] == "11" && crooms[1] == "22" && crooms[2] == "33" && crooms[3] == "44")
+  // pt 1
+  //return (crooms[0] == "11" && crooms[1] == "22" && crooms[2] == "33" && crooms[3] == "44")
+  //pt 2
+  return (crooms[0] == "1111" && crooms[1] == "2222" && crooms[2] == "3333" && crooms[3] == "4444")
 }
 
 function min(queue)
@@ -211,15 +207,6 @@ function min(queue)
 
 seen = {}
 queue = [[rooms,halls,0]]
-
-/**
-
-v = generate_legal_moves(rooms,halls)[0]
-console.log(v)
-console.log(applyMove(v,rooms,halls))
-
-*/
-
 
 while(true)
 {
